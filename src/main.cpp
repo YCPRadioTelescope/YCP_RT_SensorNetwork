4accbc224406a3c6e61d9011d2d88838818eb37b
unknown <mreinhart@ycp.edu>
2020-06-24 19:10:54 -0400

    Changed mytimer to call TimerEvent_ISR which sets the Timereventflag.

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,42 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
+int const TIMER_1MS = 1000;
+
 float Celsius = 0;
 float Fahrenheit = 0;
 
+bool TimerEventFlag = false;
 bool TempEventFlag = false;
+bool EncoderEventFlag = false;
 
-void SetTempFlag(){
-  TempEventFlag = true;
+int tempcounter = 0;
+int encodercounter =0;
+//TODO: implement other clock events
+void TimerEvent_ISR(){
+  TimerEventFlag = true;
+  
 }
 
 void setup() {
   sensors.begin();
   Serial.begin(9600);
-  myTimer.begin(SetTempFlag, 1000000);  // SetTempFlag to run every second
+  myTimer.begin(TimerEvent_ISR, TIMER_1MS);  // TimerEvent to run every millisecond
 }
 
 
 void loop() {
+
+  if(TimerEventFlag){
+    
+    tempcounter++;
+    encodercounter++;
+    //check if timer is over 1 millisecond
+    if(tempcounter >= 1){
+      tempcounter = 0;
+      TempEventFlag = true;
+    }
+  }
   if(TempEventFlag){
     TempEventFlag = false;
     sensors.requestTemperatures();
