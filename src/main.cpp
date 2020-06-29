82bc158dddfb1a670f558f9f3e8286c0d680cff7
Wyatt Wivell <wwivell@ycp.edu>
2020-06-29 10:27:54 -0400

    Added a comment

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,9 @@ void loop() {
     
     TempEventFlag = false;
     Tempsensor1.getTemp();
+
+    //Comment from Wyatt: We will need to return the data from the sensor
+    //We are just printing it, we will need to return the data after we collect it
     
     /*
     sensors.requestTemperatures();
