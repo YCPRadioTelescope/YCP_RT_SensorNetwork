8d2b896ba37c5c75b69d26cb23c7fe82c2f21b23
unknown <mreinhart@ycp.edu>
2020-07-11 22:16:19 -0400

    Reworked the ADXL345 files. used the watermark and overrun interupts to set a flag in the main file. Replaced clock interupt with external interupt for ADXL345 in superloop. Sample rate was set to 100 Hz because higher frequencies were causing the int pin to be constant.

--- src/ADXL345_Accelerometer.cpp
+++ src/ADXL345_Accelerometer.cpp
@@ -1,56 +1,214 @@
-#include <Wire.h>  // Wire library - used for I2C communication
-#include "ADXL345_Accelerometer.hpp"
-#define accel_module (0x53)
+
+#include <ADXL345_Accelerometer.hpp>         
+#include <Wire.h>
+#include <SPI.h>
+
+#define ADXL345_DEVICE (0x53)    // Device Address for ADXL345
+#define ADXL345_TO_READ (6)      // Number of Bytes Read - Two Bytes Per Axis
+
+/*********** COMMUNICATION SELECTION ***********/
+/*    Comment Out The One You Are Not Using    */
+  //ADXL345 adxl = ADXL345(10);           // USE FOR SPI COMMUNICATION, ADXL345(CS_PIN);
+  //ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
 
 
-Accelerometer::Accelerometer(){
+ADXL345::ADXL345() {
+	status = ADXL345_OK;
+	error_code = ADXL345_NO_ERROR;
 
+	gains[0] = 0.00376390;		// Original gain 0.00376390
+	gains[1] = 0.00376009;		// Original gain 0.00376009
+	gains[2] = 0.00349265;		// Original gain 0.00349265
+	I2C = true;
+}
+
+ADXL345::ADXL345(int CS) {
+	status = ADXL345_OK;
+	error_code = ADXL345_NO_ERROR;
+
+	gains[0] = 0.00376390;
+	gains[1] = 0.00376009;
+	gains[2] = 0.00349265;
+	_CS = CS;
+	I2C = false;
+	SPI.begin();
+	SPI.setDataMode(SPI_MODE3);
+	pinMode(_CS, OUTPUT);
+	digitalWrite(_CS, HIGH);
+}
+
+/******************** SETUP ********************/
+/*          Configure ADXL345 Settings         */
+void ADXL345::init(){
+
+  powerOn();                     // Power on the ADXL345
+
+  setInterruptMapping(ADXL345_INT_OVERRUNY_BIT, ADXL345_INT1_PIN);     // Map Overrun interruppt to int pin 1
+  setInterrupt(ADXL345_INT_OVERRUNY_BIT, 0);                           // Enable Overrun interrupt
+  
+  setInterruptMapping(ADXL345_INT_WATERMARK_BIT, ADXL345_INT1_PIN);    // Map Watermark interrupt to int pin 1
+  setInterrupt(ADXL345_INT_WATERMARK_BIT, 1);                          // Enable Watermark interrupt
+  
+  setRate(100);     // set sampe rate to 100 Hz
 }
 
 
-int ADXL345 = 0x53; // The ADXL345 sensor I2C address
-float X_out, Y_out, Z_out;  // Outputs
-int Accelerometer::init(){
-    Wire.begin();
-    //Serial.begin(9600);
-
-    Wire.beginTransmission(accel_module);
-    Wire.write(0x2D);
-    Wire.write(0);
-    Wire.write(16);
-    Wire.endTransmission();
-    Wire.beginTransmission(accel_module);
-    Wire.write(0x2D);
-    Wire.write(8);
-    Wire.endTransmission();
-
-    return 0;
-}
-int Accelerometer::getCords() {
-  int xyzregister = 0x32;
-  int x,y,z;
-  Wire.beginTransmission(accel_module);
-  Wire.write(xyzregister);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(accel_module);
-  Wire.requestFrom(accel_module,6);
-
-  int i =0;
-  while(Wire.available()){
-    values[i] = Wire.read();
-    i++;
+/****************** MAIN CODE ******************/
+/* Accelerometer Readings */
+void ADXL345::emptyFifo(){
+  
+  int x,y,z;   
+  //Serial.println("Starting Fifo buffer read");
+  for(int i =0; i<=32; i++){                    // loop through fifo buffer and empty it
+    readAccel(&x, &y, &z);                 // reads acceleration
+    delayMicroseconds(5);                       // minimum time between last read and start of the next read is 5 us
+    // Serial.print(x);
+    // Serial.print(", ");
+    // Serial.print(y);
+    // Serial.print(", ");
+    // Serial.println(z); 
   }
-  Wire.endTransmission();
+  Serial.println("Fifo buffer emptied");
 
-  x = (((int)values[1]) << 8) | values[0];
-  y = (((int)values[3]) << 8) | values[2];
-  z = (((int)values[5]) << 8) | values[4];
+}
 
-  sprintf(output, "%d %d %d", x, y, z);
-  Serial.print(output);
-  Serial.write(10);
 
-  return 0;
-  //delay(1000);
+
+void ADXL345::powerOn() {
+	if(I2C) {
+		Wire.begin();				// If in I2C Mode Only
+	}
+	//ADXL345 TURN ON
+	writeTo(ADXL345_POWER_CTL, 0);	// Wakeup
+	writeTo(ADXL345_POWER_CTL, 16);	// Auto_Sleep
+	writeTo(ADXL345_POWER_CTL, 8);	// Measure
+}
+
+void ADXL345::readAccel(int *x, int *y, int *z) {
+	readFrom(ADXL345_DATAX0, ADXL345_TO_READ, _buff);	// Read Accel Data from ADXL345
+
+	// Each Axis @ All g Ranges: 10 Bit Resolution (2 Bytes)
+	*x = (int16_t)((((int)_buff[1]) << 8) | _buff[0]);
+	*y = (int16_t)((((int)_buff[3]) << 8) | _buff[2]);
+	*z = (int16_t)((((int)_buff[5]) << 8) | _buff[4]);
+}
+
+//Set sample rate in Hz
+void ADXL345::setRate(double rate){
+	byte _b,_s;
+	int v = (int) (rate / 6.25);
+	int r = 0;
+	while (v >>= 1)
+	{
+		r++;
+	}
+	if (r <= 9) {
+		readFrom(ADXL345_BW_RATE, 1, &_b);
+		_s = (byte) (r + 6) | (_b & B11110000);
+		writeTo(ADXL345_BW_RATE, _s);
+	}
+}
+
+/*********************** INTERRUPT MAPPING **************************/
+/*         Set the Mapping of an Interrupt to pin1 or pin2          */
+// eg: setInterruptMapping(ADXL345_INT_DOUBLE_TAP_BIT,ADXL345_INT2_PIN);
+void ADXL345::setInterruptMapping(byte interruptBit, bool interruptPin) {
+	setRegisterBit(ADXL345_INT_MAP, interruptBit, interruptPin);
+}
+
+void ADXL345::setInterrupt(byte interruptBit, bool state) {
+	setRegisterBit(ADXL345_INT_ENABLE, interruptBit, state);
+}
+
+/***************** WRITES VALUE TO ADDRESS REGISTER *****************/
+void ADXL345::writeTo(byte address, byte val) {
+	if(I2C) {
+		writeToI2C(address, val);
+	}
+	else {
+		writeToSPI(address, val);
+	}
+}
+
+/************************ READING NUM BYTES *************************/
+/*    Reads Num Bytes. Starts from Address Reg to _buff Array        */
+void ADXL345::readFrom(byte address, int num, byte _buff[]) {
+	if(I2C) {
+		readFromI2C(address, num, _buff);	// If I2C Communication
+	}
+	else {
+		readFromSPI(address, num, _buff);	// If SPI Communication
+	}
+}
+
+/*************************** WRITE TO I2C ***************************/
+/*      Start; Send Register Address; Send Value To Write; End      */
+void ADXL345::writeToI2C(byte _address, byte _val) {
+	Wire.beginTransmission(ADXL345_DEVICE);
+	Wire.write(_address);
+	Wire.write(_val);
+	Wire.endTransmission();
+}
+
+/*************************** READ FROM I2C **************************/
+/*                Start; Send Address To Read; End                  */
+void ADXL345::readFromI2C(byte address, int num, byte _buff[]) {
+	Wire.beginTransmission(ADXL345_DEVICE);
+	Wire.write(address);
+	Wire.endTransmission();
+
+//	Wire.beginTransmission(ADXL345_DEVICE);
+// Wire.reqeustFrom contains the beginTransmission and endTransmission in it. 
+	Wire.requestFrom(ADXL345_DEVICE, num);  // Request 6 Bytes
+
+	int i = 0;
+	while(Wire.available())
+	{
+		_buff[i] = Wire.read();				// Receive Byte
+		i++;
+	}
+	if(i != num){
+		status = ADXL345_ERROR;
+		error_code = ADXL345_READ_ERROR;
+	}
+//	Wire.endTransmission();
+}
+
+/************************** WRITE FROM SPI **************************/
+/*         Point to Destination; Write Value; Turn Off              */
+void ADXL345::writeToSPI(byte __reg_address, byte __val) {
+  digitalWrite(_CS, LOW);
+  SPI.transfer(__reg_address);
+  SPI.transfer(__val);
+  digitalWrite(_CS, HIGH);
+}
+
+/*************************** READ FROM SPI **************************/
+/*                                                                  */
+void ADXL345::readFromSPI(byte __reg_address, int num, byte _buff[]) {
+  // Read: Most Sig Bit of Reg Address Set
+  char _address = 0x80 | __reg_address;
+  // If Multi-Byte Read: Bit 6 Set
+  if(num > 1) {
+  	_address = _address | 0x40;
+  }
+
+  digitalWrite(_CS, LOW);
+  SPI.transfer(_address);		// Transfer Starting Reg Address To Be Read
+  for(int i=0; i<num; i++){
+    _buff[i] = SPI.transfer(0x00);
+  }
+  digitalWrite(_CS, HIGH);
+}
+
+void ADXL345::setRegisterBit(byte regAdress, int bitPos, bool state) {
+	byte _b;
+	readFrom(regAdress, 1, &_b);
+	if (state) {
+		_b |= (1 << bitPos);  // Forces nth Bit of _b to 1. Other Bits Unchanged.
+	}
+	else {
+		_b &= ~(1 << bitPos); // Forces nth Bit of _b to 0. Other Bits Unchanged.
+	}
+	writeTo(regAdress, _b);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
+
 #include <OneWire.h>
-//#include <DallasTemperature.h>
 #include "DS18B20.hpp"
 #include "ADXL345_Accelerometer.hpp"
 
@@ -9,7 +9,12 @@
 IntervalTimer myTimer;
 
 TemperatureSensor Tempsensor1(Temp1Pin);
-Accelerometer Accelerometer1;
+ADXL345 adxl = ADXL345();
+
+
+/****************** INTERRUPT ******************/
+/*      Uncomment If Attaching Interrupt       */
+int interruptPin = 7;                 // Setup pin 7 to be the interrupt pin
 
 int const TIMER_1MS = 1000;
 
@@ -19,7 +24,7 @@ float Fahrenheit = 0;
 bool TimerEventFlag = false;
 bool TempEventFlag = false;
 bool EncoderEventFlag = false;
-bool AccelEventFlag = false;
+bool AccelEventFlag = true;
 
 int tempcounter = 0;
 int encodercounter =0;
@@ -30,48 +35,49 @@ void TimerEvent_ISR(){
   
 }
 
+/********************* ISR *********************/
+/* Look for Interrupts and Triggered Action    */
+void ADXL_ISR() {
+  AccelEventFlag = true;
+
+}
+
 void setup() {
   Serial.begin(9600);
-  Accelerometer1.init();                     // setup an accelerometer to communicate using I2C
+  adxl.init();                     // setup an accelerometer to communicate using I2C
   myTimer.begin(TimerEvent_ISR, TIMER_1MS);  // TimerEvent to run every millisecond
+
+  attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+
 }
 
 // This is the super loop where we will be keeping track of counters, setting eventflags and calling proccess base on if any event flags were set
 void loop() {
 
+  //Serial.println("starting loop");
   if(TimerEventFlag){
 
     TimerEventFlag = false; 
     tempcounter++;
     encodercounter++;
 
-    accelcounter++;         //TODO: remove
-
-    // TODO: use interupts from accelerometer to set flag
-    if(accelcounter >= 10000){
-      accelcounter =0;
-      AccelEventFlag = true;
-    }
-
     //check if temp sensors are ready to be read
     if(tempcounter >= 100){
-
+      
       tempcounter = 0;
       TempEventFlag = true;
     }
   }
 
   if(AccelEventFlag){
+    //Serial.println("reading accel");
     AccelEventFlag = false;
-    Accelerometer1.getCords();      // gets the x y and z cordnates and prints them to the serial port. TODO: add return so data can be sent to the control room
+    adxl.emptyFifo();      // gets the x y and z cordnates and prints them to the serial port. TODO: add return so data can be sent to the control room
   }
 
   if(TempEventFlag){
-
+    
     TempEventFlag = false;
-
-    //testing testing
-
     Tempsensor1.getTemp();         // gets the temperature and prints it to the serial port. TODO: add return so data can be sent to the control room
 
     
